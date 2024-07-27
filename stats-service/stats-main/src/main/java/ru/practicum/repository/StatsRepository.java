96b57bec731d00b01b2b60afaad08879b41956f0
SashaTyutyaev <sashatut2002@yandex.ru>
2024-07-28 02:43:23 +0300

    fix: add order by hits desc in statRepository

--- stats-service/stats-main/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats-service/stats-main/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -16,25 +16,29 @@ public interface StatsRepository extends JpaRepository<Hit, Integer> {
             "(s.app, s.uri, count(s.uri)) from Hit as s " +
             "where s.timestamp between ?1 and ?2 " +
             "and s.uri in ?3 " +
-            "group by s.app, s.uri")
+            "group by s.app, s.uri " +
+            "order by count(s.uri) desc ")
     List<HitForView> findHitsByTimeAndUri(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query(value = "select new ru.practicum.dto.HitForView" +
             "(s.app, s.uri, count(s.uri)) from Hit as s " +
             "where s.timestamp between ?1 and ?2 " +
-            "group by s.app, s.uri")
+            "group by s.app, s.uri " +
+            "order by count(s.uri) desc ")
     List<HitForView> findHitsByTimeWithoutUri(LocalDateTime start, LocalDateTime end);
 
     @Query(value = "select new ru.practicum.dto.HitForView" +
             "(s.app, s.uri, count(distinct s.ip)) from Hit as s " +
             "where s.timestamp between ?1 and ?2 " +
             "and s.uri in ?3 " +
-            "group by s.app, s.uri")
+            "group by s.app, s.uri " +
+            "order by count(distinct s.ip) desc ")
     List<HitForView> findHitsByTimeAndUriUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query(value = "select new ru.practicum.dto.HitForView" +
             "(s.app, s.uri, count(distinct s.ip)) from Hit as s " +
             "where s.timestamp between ?1 and ?2 " +
-            "group by s.app, s.uri")
+            "group by s.app, s.uri " +
+            "order by count(distinct  s.ip) desc ")
     List<HitForView> findHitsByTimeWithoutUriUnique(LocalDateTime start, LocalDateTime end);
 }
