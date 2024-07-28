fd9490f4eeeb2e096d8803a88d08a78a3535476f
SashaTyutyaev <sashatut2002@yandex.ru>
2024-07-28 23:20:23 +0300

    add stats-client url into application.properties

--- stats-service/stats-client/src/main/java/ru/practicum/StatsClientService.java
+++ stats-service/stats-client/src/main/java/ru/practicum/StatsClientService.java
@@ -1,5 +1,6 @@
 package ru.practicum;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.practicum.dto.HitDto;
@@ -12,13 +13,16 @@ public class StatsClientService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${service.url:http://localhost:9090/}")
+    private String serviceUrl;
+
     public HitDto saveHit(HitDto hitDto) {
-        return restTemplate.postForObject("http://localhost:9090/hit",
+        return restTemplate.postForObject(serviceUrl + "hit",
                 hitDto, HitDto.class);
     }
 
     public List<HitForView> getStats(String start, String end, List<String> uris, Boolean unique) {
-        return restTemplate.getForObject("http://localhost:9090/stats?start=" + start + "&end=" +
+        return restTemplate.getForObject(serviceUrl + "stats?start=" + start + "&end=" +
                 end + "&uris=" + uris + "&unique=" + unique, List.class);
     }
 }
