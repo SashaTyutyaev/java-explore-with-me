8bf8b88c60b6b34846c514f964f8905aca11280b
SashaTyutyaev <sashatut2002@yandex.ru>
2024-08-09 19:09:41 +0300

    fix tests

--- main-service/src/main/java/ru/practicum/compilations/CompilationService.java
+++ main-service/src/main/java/ru/practicum/compilations/CompilationService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.admin.compilations.CompilationRepository;
 import ru.practicum.admin.compilations.CompilationsEventRepository;
 import ru.practicum.admin.compilations.model.Compilation;
--- main-service/src/main/java/ru/practicum/events/PublicEventService.java
+++ main-service/src/main/java/ru/practicum/events/PublicEventService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.StatsClientService;
 import ru.practicum.dto.HitDto;
 import ru.practicum.exceptions.IncorrectParameterException;
